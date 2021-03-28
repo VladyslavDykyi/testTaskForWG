ccbf29f2a7bfd27ec617d77a0e127732d558fa88
Vladyslav Dykyi <vladislavdykyi33@gmail.com>
2021-03-28 16:02:54 +0300

    new js

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,6 +1,14 @@
+
 $(document).ready(function () {
-    $('.burger__menu').click(function (event) {
+    $('.burger__menu, .burger__menu-link').click(function () {
         $('.burger__menu,.burger__menu-box').toggleClass('active');
-        $('body').toggleClass('lock')
+        $('body').toggleClass('lock');
     });
 });
+$(document).click(function (event) {
+    if ($(event.target).closest('.burger__menu').length) return;
+    $('.header_menu').removeClass('active')
+    $('body').removeClass('lock')
+    event.stopPropagation();
+});
+
